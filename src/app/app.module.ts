2b8e1ecda75906d23f9be140ad4374655470d31c
SWerllen <583761293@qq.com>
2019-09-23 15:51:56 +0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './component/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent
+  ],
+  imports: 
